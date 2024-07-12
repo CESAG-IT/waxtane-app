92bf11c9148229558002492f146a8d1a01f05f36
Lionnel patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2024-07-12 21:00:51 +0000

    add basic autehntication

--- tags/views.py
+++ tags/views.py
@@ -3,9 +3,11 @@ from django.contrib import messages
 from django.http import HttpResponse, JsonResponse
 from tags.models import HashTag
 from .forms import TagForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
 def index(request):
 
     tag_list = HashTag.objects.all()
@@ -16,6 +18,7 @@ def index(request):
     
     return render(request, "tags/index.html", context)
 
+@login_required(login_url='login')
 def create(request):
     if request.method == "POST":
 
@@ -38,6 +41,7 @@ def create(request):
 
     return HttpResponse("creation des tags")
 
+@login_required(login_url='login')
 def show(request,id):
     tag = HashTag.objects.get(pk=id) # SELECT * FROM has-tags WHERE id = id
 
@@ -48,9 +52,11 @@ def show(request,id):
     }
     return render(request, "tags/show.html", context)
 
+@login_required(login_url='login')
 def update(request,id):
     return HttpResponse(f"modification du tag {id}")
 
+@login_required(login_url='login')
 def delete(request,id):
     tag = HashTag.objects.get(pk=id)
     tag.delete()
